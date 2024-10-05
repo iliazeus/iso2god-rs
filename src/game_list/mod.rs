e18567c3f081f99fba758db498ccb4e3a58b77b1
astarivi <astarivi@aol.com>
2024-10-04 21:26:00 -0500

    Remove unused "hex" dependency

--- src/game_list/mod.rs
+++ src/game_list/mod.rs
@@ -1,6 +1,7 @@
 // DO NOT EDIT; this file is auto-generated.
 // Run `./generate.bash titles.jsonl mod.rs` to re-generate.
 
+
 pub fn find_title_by_id(title_id: u32) -> Option<String> {
     GAMES_BY_TITLE_ID
         .binary_search_by_key(&title_id, |x| x.0)
